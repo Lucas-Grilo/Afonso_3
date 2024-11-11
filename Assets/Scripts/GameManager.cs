5fa50e98359230de0007aea286d5af15751d40e2
Lucas-Grilo <lucas.noronha1979@gmail.com>
2024-11-10 21:12:05 -0300

    up

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,30 +2,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; }
-    public InputManager InputManager { get; private set; }  // Referência ao InputManager
-    public AudioManager AudioManager { get; private set; }  // Referência ao AudioManager
+    public static GameManager Instance;
+    public InputManager InputManager { get; private set; }
+    public AudioManager AudioManager;
 
     private void Awake()
     {
-        if (Instance != null)
-        {
-            Destroy(gameObject);  // Evita instância duplicada
-            return;
-        }
-
+        if (Instance != null) Destroy(this.gameObject);
         Instance = this;
 
-        DontDestroyOnLoad(gameObject);  // Mantém o GameManager entre cenas
-
-        // Acessando a instância do InputManager via Singleton
-        InputManager = InputManager.Instance;
-
-        // Acessando o AudioManager, caso seja necessário
-        AudioManager = FindObjectOfType<AudioManager>();  
-        if (AudioManager == null)
-        {
-            Debug.LogError("AudioManager não encontrado na cena.");
-        }
+        InputManager = new InputManager();
     }
 }
--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -3,21 +3,6 @@ using UnityEngine.InputSystem;
 
 public class InputManager
 {
-    private static InputManager _instance;
-
-    // Propriedade para acessar a instância única
-    public static InputManager Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = new InputManager(); // Cria a instância se não existir
-            }
-            return _instance;
-        }
-    }
-
     private PlayerControls playerControls;
 
     public float Movement => playerControls.Gameplay.Movement.ReadValue<float>();
@@ -25,9 +10,9 @@ public class InputManager
     public event Action OnJump;
     public event Action OnAttack;
     public event Action OnSlide;
-    public event Action<bool> OnCrouch;
+    public event Action<bool> OnCrouch;  // Evento para agachar com true (iniciar) e false (parar)
 
-    private InputManager()
+    public InputManager()
     {
         playerControls = new PlayerControls();
         EnablePlayerInput();
@@ -35,30 +20,38 @@ public class InputManager
         playerControls.Gameplay.Jump.performed += OnJumpPerformed;
         playerControls.Gameplay.Attack.performed += OnAttackPerformed;
         playerControls.Gameplay.Slide.performed += OnSlidePerformed;
-        playerControls.Gameplay.Crouch.started += OnCrouchStarted;
-        playerControls.Gameplay.Crouch.canceled += OnCrouchCanceled;
+
+        // Agachar (pressionado ou solto)
+        playerControls.Gameplay.Crouch.started += OnCrouchStarted;   // Quando começar a agachar
+        playerControls.Gameplay.Crouch.canceled += OnCrouchCanceled; // Quando parar de agachar
     }
 
-    private void OnJumpPerformed(InputAction.CallbackContext context) => OnJump?.Invoke();
-    private void OnAttackPerformed(InputAction.CallbackContext context) => OnAttack?.Invoke();
-    private void OnSlidePerformed(InputAction.CallbackContext context) => OnSlide?.Invoke();
-    private void OnCrouchStarted(InputAction.CallbackContext context) => OnCrouch?.Invoke(true);
-    private void OnCrouchCanceled(InputAction.CallbackContext context) => OnCrouch?.Invoke(false);
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        OnJump?.Invoke();
+    }
+
+    private void OnAttackPerformed(InputAction.CallbackContext context)
+    {
+        OnAttack?.Invoke();
+    }
+
+    private void OnSlidePerformed(InputAction.CallbackContext context)
+    {
+        OnSlide?.Invoke();
+    }
+
+    private void OnCrouchStarted(InputAction.CallbackContext context)
+    {
+        OnCrouch?.Invoke(true);  // Inicia o agachamento
+    }
+
+    private void OnCrouchCanceled(InputAction.CallbackContext context)
+    {
+        OnCrouch?.Invoke(false);  // Encerra o agachamento
+    }
 
     public void DisablePlayerInput() => playerControls.Gameplay.Disable();
+
     public void EnablePlayerInput() => playerControls.Gameplay.Enable();
-
-    public void Cleanup()
-    {
-        playerControls.Gameplay.Jump.performed -= OnJumpPerformed;
-        playerControls.Gameplay.Attack.performed -= OnAttackPerformed;
-        playerControls.Gameplay.Slide.performed -= OnSlidePerformed;
-        playerControls.Gameplay.Crouch.started -= OnCrouchStarted;
-        playerControls.Gameplay.Crouch.canceled -= OnCrouchCanceled;
-
-        playerControls.Gameplay.Disable();
-        playerControls = null;
-    }
-
-    ~InputManager() => Cleanup();
 }
